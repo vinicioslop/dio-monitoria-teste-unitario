49fab1981a4abefbbb9eb9674e220f36048e8024
vinicioslop <vinicioslop7@hotmail.com>
2021-10-31 17:03:15 -0300

    Sobe os códigos feitos com o projeto base básico

---
+++ Calculator/Calculator.Testes/CalculatorTests.cs
@@ -0,0 +1,52 @@
+﻿using Shouldly;
+using System;
+using Xunit;
+
+namespace Calculator.Testes
+{
+    public class CalculatorTests
+    {
+        // AAA
+        // Arrange - Configurações
+        // Act     - Execução
+        // Assert  - Verificação
+        
+        [Fact]
+        public void Add_ShouldReturnTheCorrectSum()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var result = calculator.Add(1, 2);
+
+            // Assert
+            result.ShouldBe(3);
+        }
+
+        [Theory]
+        [InlineData(2, 1, 1)]
+        [InlineData(5, 2, 3)]
+        public void Subtract_ShouldReturnTheCorrectSubtraction(int number1, int number2, int expectedResult)
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var result = calculator.Subtract(number1, number2);
+
+            // Assert
+            result.ShouldBe(expectedResult);
+        }
+
+        [Fact]
+        public void Divide_ShouldThrowExceptionWhenDividingByZero()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act & Assert
+            Should.Throw<Exception>(() => calculator.Divide(4, 0));
+        }
+    }
+}
---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+        public int Subtract(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+        public int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+        public double Divide(int number1, int number2)
+        {
+            if (number2 == 0)
+                throw new Exception("Não é possível dividir por 0!");
+
+            return number1 / number2;
+        }
+    }
+}
